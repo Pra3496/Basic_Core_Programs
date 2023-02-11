442330056bf88db542d77f03cc4c14444c3c6dfc
Pranav Waghmare <79601856+Pra3496@users.noreply.github.com>
2023-02-11 20:59:35 +0530

    Add files via upload

---
+++ Basic Core Progams/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Basic_Core_Progams
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+
+
+
+            Console.Write("Please enter input value : ");
+            int iNo = Convert.ToInt32(Console.ReadLine());
+            Factor fact = new Factor();
+            fact.getFactors(iNo);
+
+
+
+            
+
+        }
+    }
+}
