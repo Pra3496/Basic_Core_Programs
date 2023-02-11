14cc66ceced5cb5b65c1d928a8261367dacde409
Pranav Waghmare <79601856+Pra3496@users.noreply.github.com>
2023-02-11 20:52:21 +0530

    Add files via upload

---
+++ Basic Core Progams/HarmonicNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Progams
+{
+    internal class HarmonicNumber
+    {
+        public static void getHarmonicNumber()
+        {
+            Console.WriteLine("Please enter range for Harmonics");
+            double range = Convert.ToInt32(Console.ReadLine());
+            double value = 1;
+            for (double dNo = 1; dNo <= range; dNo++)
+            {
+                value = value + (1 / value);
+            }
+            Console.WriteLine("The harmonic series of number is:" + value);
+        }
+    }
+}
