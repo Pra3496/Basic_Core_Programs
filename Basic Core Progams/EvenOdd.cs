751d91b758526114f46be51650ed6c016a0c7062
Pranav Waghmare <79601856+Pra3496@users.noreply.github.com>
2023-02-11 20:51:05 +0530

    Add files via upload

---
+++ Basic Core Progams/EvenOdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Progams
+{
+    internal class EvenOdd
+    {
+        public static void getEvenOdd()
+        {
+            Console.Write("Please enter a number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The number is even");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd");
+            }
+        }
+    }
+}
