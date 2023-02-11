02207cd6933801ef9063ff0c3ae18db9193d703f
Pranav Waghmare <79601856+Pra3496@users.noreply.github.com>
2023-02-11 17:55:53 +0530

    Flip a coin

--- Basic Core Progams/FlipCoin.cs
+++ Basic Core Progams/FlipCoin.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,25 +8,29 @@ namespace Basic_Core_Progams
 {
     internal class FlipCoin
     {
-        public static void flipCoin()      //Method created
+        public static void getFlipCoin()      //Method created
         {
-            int tailCount = 0, headCount = 0;       //Initialise values
-            Console.WriteLine("Number of times to the flipcoin");       //Getting  number of count head and tail
-            int n = Convert.ToInt32(Console.ReadLine());
+            int tail_count = 0, head_count = 0;       //Initialise values
+            Console.Write("Number of times to the flipcoin : ");       //Getting  number of count head and tail
+            int iNo = Convert.ToInt32(Console.ReadLine());
             Random random = new Random();       //Random object is created
 
-            for (int i = 1; i <= n; i++)        //For loop for given condition
+            for (int i = 1; i <= iNo; i++)        //For loop for given condition
             {
                 double input = random.NextDouble();     //Using random function
                 if (input < 0.5)        //Providing some condition statements
-                    tailCount++;
+                {
+                    tail_count++;
+                }
                 else
-                    headCount++;
+                {
+                    head_count++;
+                }
             }
-            Console.WriteLine("TailCount:{0} and HeadCount:{1}", tailCount, headCount);
-            double tailPercentage = (tailCount * 100) / n;      //Percentage calculating 
-            double headPercentage = (headCount * 100) / n;      //percentage calculating
-            Console.WriteLine("Tail={0}% and Head={1}%", tailPercentage, headPercentage);
+            Console.WriteLine("TailCount:{0} and HeadCount:{1}",tail_count,head_count);
+            double tail_percentage = (tail_count * 100) / iNo;      //Percentage calculating 
+            double head_percentage = (head_count * 100) / iNo;      //percentage calculating
+            Console.WriteLine("Tail={0}% and Head={1}%", tail_percentage, head_percentage);
         }
     }
 }
