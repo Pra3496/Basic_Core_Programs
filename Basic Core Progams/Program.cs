490d738c539d8831d41777dfa1eb5e3d5a87741f
Pranav Waghmare <79601856+Pra3496@users.noreply.github.com>
2023-02-12 00:05:20 +0530

    Vowels and Consonents

---
+++ Basic Core Progams/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Basic_Core_Progams
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+
+            Vowels vowel = new Vowels();
+            vowel.getVowelConsonant();
+
+        }
+    }
+}
