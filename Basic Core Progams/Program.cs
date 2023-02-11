e242f68aa32f250802bdd169cef96b12ff57110b
Pranav Waghmare <79601856+Pra3496@users.noreply.github.com>
2023-02-11 21:06:22 +0530

    Add files via upload

---
+++ Basic Core Progams/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Basic_Core_Progams
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+
+            LargestNumber largenumber = new LargestNumber();
+            largenumber.getLargestNumber();
+
+
+        }
+    }
+}
