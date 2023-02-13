dfd9b78a586e00726557538bfd2010f42be59d96
Pranav Waghmare <piyushw55@gmail.com>
2023-02-13 13:09:56 +0530

    Final Application Version 2.0.0

---
+++ Basic Core Progams/MainPanal.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics.X86;
+using System.Xml.Linq;
+
+namespace Basic_Core_Progams
+{
+    internal class MainPanal
+    {
+        string name1 = "",gtype1="";
+        int iCnt;
+
+
+       
+        //////////////////////////// Main Menu Settings /////////////////////////////////////////
+        
+        public void setMainMenu(string name)
+        {
+            Console.WriteLine("\n");
+            string nam = "WEL-COME TO "+name+" Application";
+
+            this.gtype1 = "*";
+            this.name1 = nam;
+
+            printGrid();
+            Console.WriteLine();
+            printGrid(nam);
+            Console.WriteLine();
+            printGrid();
+            Console.WriteLine();
+
+        }
+
+        private void printGrid(string nam)
+        {
+            int i;
+
+            for (i = 0; i < nam.Length; i++)
+            {
+                if (i == (nam.Length / 2))
+                {
+                    Console.Write("|{0}|", nam);
+                }
+
+                Console.Write(this.gtype1);
+
+            }
+
+
+        }
+
+        private void printGrid()
+        {
+
+            for (int i = 0; i < this.name1.Length + 1; i++)
+            {
+
+                Console.Write(this.gtype1 + "" + this.gtype1);
+            }
+
+        }
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //Option in Main Menu Settings
+
+
+        public void setOptMenu(string[] options)
+        {
+            string name = "OPTIONS ARE FOLLOWING";
+
+            int i;
+            for (i = 0; i < name.Length; i++)
+            {
+                if (i == (name.Length / 2))
+                {
+                    Console.Write("|{0}|", name);
+                }
+
+                Console.Write(this.gtype1 + "" + this.gtype1+"" + this.gtype1);
+
+            }
+            Console.WriteLine();
+            this.iCnt = i;
+
+            for(int k = 0;k < options.Length;k++)
+            {
+                Console.WriteLine("{0}: {1}", k+1, options[k]);
+            }
+
+            Console.WriteLine();
+            for (int j = 0; j < this.iCnt + 2; j++)
+            {
+                if (j == (this.iCnt / 2))
+                {
+                    Console.Write("|END OF OPTIONS|");
+                }
+
+                Console.Write(this.gtype1 + "" + this.gtype1 + "" + this.gtype1);
+
+            }
+            Console.WriteLine();
+
+            
+        }
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+        //Sub Menu Settings
+        public void setSubMenuEty(string name,string gType)
+        {
+            this.name1 = name;
+            this.gtype1 = gType;
+
+            int i;
+            for (i = 0; i < name.Length; i++)
+            {
+                if (i == (name.Length / 2))
+                {
+                    Console.Write("|{0}|", name);
+                }
+
+                Console.Write(this.gtype1);
+
+            }
+            Console.WriteLine((i*gType.Length) + i + 2);
+
+            this.iCnt = (i * gType.Length) +i + 2;
+            string g = gType[0] + "";
+            this.gtype1 = g;
+        }
+
+        public void printSubGrid()
+        {
+            Console.WriteLine(this.iCnt);
+            for (int i = 0; i < this.iCnt-5; i++)
+            {
+                if (i == ((this.iCnt-5) / 2))
+                {
+                    Console.Write("|END|");
+                }
+
+                Console.Write(this.gtype1);
+
+            }
+            Console.WriteLine();
+
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    }
+}
--- Basic Core Progams/Program.cs
+++ Basic Core Progams/Program.cs
@@ -1,148 +1,115 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-﻿namespace Basic_Core_Progams
-{
-    internal class Program
-    {
-       
-        static void Main(string[] args)
-        {
 
-            Vowels vowel = new Vowels();
-            vowel.getVowelConsonant();
-
-        }
-    }
-=======
->>>>>>> FinalApp
-﻿namespace Basic_Core_Progams
+namespace Basic_Core_Progams
 {
-    internal class Program
-    {
-       
-        static void Main(string[] args)
-        {
-            namespace Basic_Core_Progams
-    {
+     
         internal class Program
         {
 
             static void Main(string[] args)
             {
+            
                 char cnt = '\0';
-
-
+                string[] opt = { "FlipCoin", "LeapYear", "PowerOfTwo", "Harmonics", "Factors", "QuotientAn", "SwapTwoNumbers", "EvenOddNumber", "VowelConsonant", "LargestNumber" };
+                MainPanal panal = new MainPanal();
                 while (true)
-                {
-                Menu:
-                    Console.WriteLine("\n***********************************************************************");
-                    Console.WriteLine("***********|  WEL-COME TO BASIC CORE PROGRAMS Application  |***********");
-                    Console.WriteLine("***********************************************************************");
-                    Console.WriteLine("*********************  OPTIONS ARE FOLLOWING  *************************\n");
-                    Console.WriteLine("1:FlipCoin\n2:LeapYear\n3:PowerOfTwo\n4:Harmonics\n5:Factors\n6:QuotientAndRemainder\n7:SwapTwoNumbers\n8:EvenOddNumber\n9:VowelConsonant\n10:LargestNumber\n11:Prime Factorization");
-                    Console.WriteLine("\n*************************|  END OF OPTIONS  |*************************");
-                    Console.Write("Enter the Option : ");
-                    int option = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine();
-                    Console.Clear();
-
-                    switch (option)
                     {
-
-                        case 1:
-                            Console.WriteLine("\n*******************> Result <*********************\n");
-                            FlipCoin.getFlipCoin();
-                            Console.WriteLine("\n****************> End of Result <******************\n");
-                            break;
-                        case 2:
-                            Console.WriteLine("\n*******************> Result <*********************\n");
-                            LeapYear.getLeapYear();
-                            Console.WriteLine("\n****************> End of Result <******************\n");
-                            break;
-                        case 3:
-                            Console.WriteLine("\n*******************> Result <*********************\n");
-                            PowerOfTwo.getPowerOfTwoNum();
-                            Console.WriteLine("\n****************> End of Result <******************\n");
-                            break;
-                        case 4:
-                            Console.WriteLine("\n*******************> Result <*********************\n");
-                            HarmonicNumber.getHarmonicNumber();
-                            Console.WriteLine("\n****************> End of Result <******************\n");
-                            break;
-                        case 5:
-                            Console.WriteLine("\n*******************> Result <*********************\n");
-                            Console.Write("Please enter input value : ");
-                            int iNo = Convert.ToInt32(Console.ReadLine());
-                            Factor fact = new Factor();
-                            fact.getFactors(iNo);
-                            Console.WriteLine("\n****************> End of Result <******************\n");
-                            break;
-                        case 6:
-                            Console.WriteLine("\n*******************> Result <*********************\n");
-                            QuoitAndReminder quoitRem = new QuoitAndReminder();
-                            quoitRem.getQuotientAndReminder();
-                            Console.WriteLine("\n****************> End of Result <******************\n");
-                            break;
-                        case 7:
-                            Console.WriteLine("\n*******************> Result <*********************\n");
-                            SwapNumber swapnumber = new SwapNumber();
-                            swapnumber.getSwapTwoNumbers();
-                            Console.WriteLine("\n****************> End of Result <******************\n");
-                            break;
-                        case 8:
-                            Console.WriteLine("\n*******************> Result <*********************\n");
-                            EvenOdd.getEvenOdd();
-                            Console.WriteLine("\n****************> End of Result <******************\n");
-                            break;
-                        case 9:
-                            Console.WriteLine("\n*******************> Result <*********************\n");
-                            Vowels vowel = new Vowels();
-                            vowel.getVowelConsonant();
-                            Console.WriteLine("\n****************> End of Result <******************\n");
-                            break;
-                        case 10:
-                            Console.WriteLine("\n*******************> Result <*********************\n");
-                            LargestNumber largenumber = new LargestNumber();
-                            largenumber.getLargestNumber();
-                            Console.WriteLine("\n****************> End of Result <******************\n");
-                            break;
-                        default:
-                            Console.WriteLine("Please enter right option !!!!!!!!!!!!!");
-                            goto Menu;
-                    }
-                ExitMenu:
-
-                    Console.WriteLine("\nEnter Y to Continue OR N to Exit the Application\n");
-                    Console.Write("(Y/N)=>\t");
-                    cnt = Console.ReadLine()[0];
-                    Console.WriteLine("\n");
-                    if (('Y'.Equals(cnt)) || ('y'.Equals(cnt)))
-                    {
+                    Menu:
                         Console.Clear();
-                        continue;
-                    }
-                    else if (('n'.Equals(cnt)) || ('N'.Equals(cnt)))
-                    {
-                        Console.Clear();
-                        Console.WriteLine("\n\n\n**************>| THANK YOU for using this application |<***************");
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("\nEnter the Proper Option !!!!!!!!!!!!!!!!!!!!!");
-                        goto ExitMenu;
-                    }
+                        panal.setMainMenu("Basic Core Program");
+                        panal.setOptMenu(opt);
+                        Console.Write("Enter the Option : ");
+                        int option = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine();
+
+                        switch (option)
+                        {
+
+                            case 1:
+                                panal.setSubMenuEty("FlipCoin","====");
+                                FlipCoin.getFlipCoin();
+                                panal.printSubGrid();
+                                break;
+                            case 2:
+                                panal.setSubMenuEty("LeapYear","====");
+                                LeapYear.getLeapYear();
+                                panal.printSubGrid();
+                                break;
+                            case 3:
+                                panal.setSubMenuEty("PowerOfTwo","====");
+                                PowerOfTwo.getPowerOfTwoNum();
+                                panal.printSubGrid();
+                                break;
+                            case 4:
+                                panal.setSubMenuEty("Harmonics","====");
+                                HarmonicNumber.getHarmonicNumber();
+                                panal.printSubGrid();
+                                break;
+                            case 5:
+                                panal.setSubMenuEty("Factors","====");
+                                Console.Write("Please enter input value : ");
+                                int iNo = Convert.ToInt32(Console.ReadLine());
+                                Factor fact = new Factor();
+                                fact.getFactors(iNo);
+                                panal.printSubGrid();
+                                break;
+                            case 6:
+                                panal.setSubMenuEty("QuoitAndReminder", "====");
+                                QuoitAndReminder quoitRem = new QuoitAndReminder();
+                                quoitRem.getQuotientAndReminder();
+                                panal.printSubGrid();
+                                break;
+                            case 7:
+                                panal.setSubMenuEty("SwapTwoNumbers","====");
+                                SwapNumber swapnumber = new SwapNumber();
+                                swapnumber.getSwapTwoNumbers();
+                                panal.printSubGrid();
+                                break;
+                            case 8:
+                                panal.setSubMenuEty("EvenOddNumber","====");
+                                EvenOdd.getEvenOdd();
+                                panal.printSubGrid();
+                                break;
+                            case 9:
+                                panal.setSubMenuEty("VowelConsonant","====");
+                                Vowels vowel = new Vowels();
+                                vowel.getVowelConsonant();
+                                panal.printSubGrid();
+                                break;
+                            case 10:
+                                panal.setSubMenuEty("LargestNumber","====");
+                                LargestNumber largenumber = new LargestNumber();
+                                largenumber.getLargestNumber();
+                                panal.printSubGrid();
+                                break;
+                            default:
+                                Console.WriteLine("Please enter right option !!!!!!!!!!!!!");
+                                goto Menu;
+                        }
+                    ExitMenu:
+
+                        Console.WriteLine("\nEnter Y to Continue OR N to Exit the Application\n");
+                        Console.Write("(Y/N)=>\t");
+                        cnt = Console.ReadLine()[0];
+                        Console.WriteLine("\n");
+                        if (('Y'.Equals(cnt)) || ('y'.Equals(cnt)))
+                        {
+                            Console.Clear();
+                            continue;
+                        }
+                        else if (('n'.Equals(cnt)) || ('N'.Equals(cnt)))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("\n\n\n**************>| THANK YOU for using this application |<***************");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nEnter the Proper Option !!!!!!!!!!!!!!!!!!!!!");
+                            goto ExitMenu;
+                        }
 
                 }
             }
         }
-    }
-
-}
-    }
-<<<<<<< HEAD
-=======
->>>>>>> main
->>>>>>> FinalApp
+ 
 }
